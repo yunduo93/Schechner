203063a94c98a30720db5cfa2037d459bb48110b
yunduo93 <37552283+yunduo93@users.noreply.github.com>
2019-05-02 19:18:27 +0800

    Update schechner.m

--- schechner.m
+++ schechner.m
@@ -3,51 +3,48 @@
 
 clc,clear all,close all
 % addpath('./Dehaze_Code/images');
-I_90 = im2double(imread('por90_2.jpg'));
-I_0 = im2double(imread('por0_2.jpg'));
+[fname pname]=uigetfile({'*.png;*.jpg;*.tif','Figure(*.png,*.jpg,*.tif)'},'pick a file','F:\0-动笔系列\pic');
+image1= imread([pname,fname]);
+I_90 = im2double(image1);
+[fname2 pname2]=uigetfile({'*.png;*.jpg;*.tif','Figure(*.png,*.jpg,*.tif)'},'pick a file','F:\0-动笔系列\pic');
+image2= imread([pname2,fname2]);
+I_0 = im2double(image2);
+
 Imin = I_0-I_90;
 Imax = I_0+I_90;
-figure,imshow(Imax),title('I');
+% figure,imshow(Imax),title('I');
 p = Imin./Imax;  % 0.7附近
 % pmean = mean2(p)
 
 %% p,A0
 e = 1.3;
 A = Imin./(p*e);
-figure,imshow(A,[])
 Amax = max(max(Imax));
-% D_tmp = Imax-A*e;
 
 D_tmp = Imax-A;
-figure,imshow(D_tmp,[])
-
-for i=1:3
     if(D_tmp<=0)
         D_tmp=nlfilter(D_tmp,[5 5],@mean_negtive);
     end
-end
 D_tmp=max(D_tmp,0);
 figure,imshow(D_tmp);title('D_tmp');
+
 t = 1-A./Amax;
-t = max(t,0.01);
-for i=1:3
+t = max(t,0.01);  % t = max(t,0.01);
     if(t<=0)
         t=nlfilter(t,[5 5],@mean_negtive);
     end
-end
 figure,imshow(t);title('t');
 
 L = D_tmp./t;
+figure,imshow(L,[])
 
-figure,imshow(L),title('L');
-imwrite(L,'L.jpg');
+%% save the result
+% fpath=pwd; % save current dialoge
+% cd('C:\Users\zy128\Documents\GitHub\Single-Polarized-Imaging\result表3.1'); % change to the designate dialoge
+% % mkdir result表3.1  % If don't have a folder, then New Folder
+% imwrite(L,'L2_yy.jpg');
+% cd(fpath)  % back to current dialoge
 
-%% read the area
-pos = load('pos_xian.mat');
-pos = pos.pos;
-I_crop = imcrop(L,pos);
-figure,imshow(I_crop,[]);
-imwrite(I_crop,'y_crop.jpg');
 %% calculate the EME
 % k1 = 10;
 % k2 = 10;
